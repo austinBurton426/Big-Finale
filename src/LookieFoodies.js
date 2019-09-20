1bc09bbc48d6e6d2feac033f1367152a2721dbae
Austin Burton <austin.burton426@gmail.com>
2019-09-20 11:25:21 -0600

    yay

---
+++ src/LookieFoodies.js
@@ -0,0 +1,36 @@
+// to view object, ill put into a modal
+
+import React from "react";
+
+class LookieFoodie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Meal: this.props.data.Title,
+      Time: this.props.data.Time,
+      Calories: this.props.data.Calories,
+      TotalFat: this.props.data.TotalFat,
+      Sugar: this.props.data.Sugar,
+      Protein: this.props.data.Protein,
+      Description: this.props.data.Description,
+      currentDate: new Date().toDateString()
+    };
+  }
+
+  render() {
+    return (
+         <div>
+           <h1>{this.state.Description}</h1>
+             <h2>{this.state.MealType}</h2>
+             Date: {this.state.Meal}<br/>
+             Calories: {this.state.Calories}<br/>
+             Total Fat: {this.state.TotalFat}<br/>
+             Sugar: {this.state.Sugar}<br/>
+             Protein: {this.state.Protein}
+         </div>
+    );
+  }
+}
+
+export default LookieFoodie;
---
+++ src/config/coms.js
@@ -0,0 +1,1 @@
+export const FOODS_API = process.env.REACT_APP_FOODS_API || "https://foodie-api5000.herokuapp.com"
